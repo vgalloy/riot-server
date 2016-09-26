f24831d3476732b9a431ff616d3711b28b1c596b
Vincent Galloy <vgalloy@excilys.com>
2016-09-26 20:06:09 +0200

    [ FIX ] : Last game now order result

--- riot-server-dao/src/main/java/vgalloy/riot/server/dao/internal/dao/commondao/impl/MatchDetailDaoImpl.java
+++ riot-server-dao/src/main/java/vgalloy/riot/server/dao/internal/dao/commondao/impl/MatchDetailDaoImpl.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.mongodb.BasicDBObject;
 import org.mongojack.DBCursor;
 import org.mongojack.DBQuery;
 
 import vgalloy.riot.api.api.constant.Region;
 import vgalloy.riot.api.api.dto.mach.MatchDetail;
-import vgalloy.riot.server.dao.internal.dao.commondao.AbstractCommonDao;
 import vgalloy.riot.server.dao.api.dao.MatchDetailDao;
+import vgalloy.riot.server.dao.internal.dao.commondao.AbstractCommonDao;
 import vgalloy.riot.server.dao.internal.entity.dataobject.DataObject;
 import vgalloy.riot.server.dao.internal.entity.dataobject.MatchDetailDo;
 
@@ -37,6 +38,7 @@ public final class MatchDetailDaoImpl extends AbstractCommonDao<MatchDetail, Mat
         List<MatchDetail> result = new ArrayList<>();
         DBCursor<MatchDetailDo> queryResult = collection.find(DBQuery.is("item.participantIdentities.player.summonerId", summonerId))
                 .and(DBQuery.is("region", region))
+                .sort(new BasicDBObject("item.matchCreation", -1))
                 .limit(limit);
         result.addAll(queryResult.toArray().stream().map(DataObject::getItem).collect(Collectors.toList()));
         return result;
