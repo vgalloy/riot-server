0081c3e4fd9f27151600aa3284bb90cebbd16068
Vincent Galloy <vgalloy@excilys.com>
2016-09-30 23:10:23 +0200

    [ NEW ] : Add log for request timing

--- riot-server-webservice/src/main/java/vgalloy/riot/server/webservice/internal/filter/IpLogFilter.java
+++ riot-server-webservice/src/main/java/vgalloy/riot/server/webservice/internal/filter/IpLogFilter.java
@@ -1,18 +1,18 @@
 package vgalloy.riot.server.webservice.internal.filter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-import org.springframework.stereotype.Component;
-
+import java.io.IOException;
+import java.util.UUID;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import java.io.IOException;
-import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Vincent Galloy - 19/09/16
@@ -34,7 +34,10 @@ public class IpLogFilter implements Filter {
         MDC.put("requestUuid", requestUuid);
 
         LOGGER.info("ip : {}", servletRequest.getRemoteAddr());
+
+        long startTimeNano = System.nanoTime();
         filterChain.doFilter(servletRequest, servletResponse);
+        LOGGER.info("execution time : {}", System.nanoTime() - startTimeNano);
     }
 
     @Override
