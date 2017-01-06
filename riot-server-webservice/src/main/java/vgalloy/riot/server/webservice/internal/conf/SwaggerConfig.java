4724fdc2e0e2ea26fc620f89cfdec0baa2e6ce2c
Vincent Galloy <vgalloy@excilys.com>
2017-01-06 17:30:22 +0100

    [ FIX ] : Response status code in Swagger

--- riot-server-webservice/src/main/java/vgalloy/riot/server/webservice/internal/conf/SwaggerConfig.java
+++ riot-server-webservice/src/main/java/vgalloy/riot/server/webservice/internal/conf/SwaggerConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -40,11 +41,12 @@ public class SwaggerConfig {
                 .directModelSubstitute(LocalDate.class, String.class)
                 .directModelSubstitute(SummonerId.class, String.class)
                 .directModelSubstitute(GameId.class, String.class)
-//                .genericModelSubstitutes(ResponseEntity.class)
+                .genericModelSubstitutes(ResponseEntity.class)
                 .useDefaultResponseMessages(false)
                 .globalResponseMessage(RequestMethod.GET, Arrays.asList(
                         new ResponseMessageBuilder().code(200).message("OK").responseModel(new ModelRef("Success")).build(),
                         new ResponseMessageBuilder().code(202).message("Resource not loaded yet. Retry Later").responseModel(new ModelRef("Accepted")).build(),
+                        new ResponseMessageBuilder().code(400).message("Bad request").responseModel(new ModelRef("Error")).build(),
                         new ResponseMessageBuilder().code(401).message("Unauthorized").responseModel(new ModelRef("Error")).build(),
                         new ResponseMessageBuilder().code(404).message("Resource not found").responseModel(new ModelRef("Error")).build(),
                         new ResponseMessageBuilder().code(500).message("Internal Server Error").responseModel(new ModelRef("Error")).build()));
