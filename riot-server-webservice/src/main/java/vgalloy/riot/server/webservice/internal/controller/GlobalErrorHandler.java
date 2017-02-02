8ecdc7afa3e4ba00e27a75b52d30a9f48d940446
Vincent Galloy <vgalloy@excilys.com>
2017-02-02 19:52:13 +0100

    [ CHG ] : Factorise @ResponseBody

--- riot-server-webservice/src/main/java/vgalloy/riot/server/webservice/internal/controller/GlobalErrorHandler.java
+++ riot-server-webservice/src/main/java/vgalloy/riot/server/webservice/internal/controller/GlobalErrorHandler.java
@@ -19,6 +19,7 @@ import vgalloy.riot.server.webservice.internal.model.ResourceNotLoadedException;
  * @author Vincent Galloy
  *         Created by Vincent Galloy on 24/08/16.
  */
+@ResponseBody
 @ControllerAdvice
 public final class GlobalErrorHandler {
 
@@ -30,10 +31,9 @@ public final class GlobalErrorHandler {
      * @param e The handle exception
      * @return The error message for web user
      */
-    @ResponseBody
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Throwable.class)
-    public Error handleException(Throwable e) {
+    public Error handle(Throwable e) {
         LOGGER.error("", e);
         return new Error(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Ups ... unexpected error occurred ! !");
     }
@@ -44,10 +44,9 @@ public final class GlobalErrorHandler {
      * @param e The handle exception
      * @return The error message for web user
      */
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public Error handleArgumentMismatchException(MethodArgumentTypeMismatchException e) {
+    public Error handle(MethodArgumentTypeMismatchException e) {
         LOGGER.error("{}", e.getMessage());
         return new Error(HttpStatus.BAD_REQUEST.value(), "Value of '" + e.getName() + "' can not be convert into [" + e.getRequiredType().getSimpleName() + "]");
     }
@@ -58,10 +57,9 @@ public final class GlobalErrorHandler {
      * @param e The handle exception
      * @return The error message for web user
      */
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MissingServletRequestParameterException.class)
-    public Error handleMissingParameterException(MissingServletRequestParameterException e) {
+    public Error handle(MissingServletRequestParameterException e) {
         LOGGER.error("{}", e.getMessage());
         return new Error(HttpStatus.BAD_REQUEST.value(), "The parameter : " + e.getParameterName() + " is mandatory.");
     }
@@ -72,10 +70,9 @@ public final class GlobalErrorHandler {
      * @param e The handle exception
      * @return The error message for web user
      */
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserException.class)
-    public Error handleServiceException(UserException e) {
+    public Error handle(UserException e) {
         LOGGER.error("{}", e.getMessage());
         return new Error(HttpStatus.BAD_REQUEST.value(), e.getMessage());
     }
@@ -86,10 +83,9 @@ public final class GlobalErrorHandler {
      * @param e The handle exception
      * @return The error message for web user
      */
-    @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ResourceDoesNotExistException.class)
-    public Error handleServiceException(ResourceDoesNotExistException e) {
+    public Error handle(ResourceDoesNotExistException e) {
         return new Error(HttpStatus.NOT_FOUND.value(), ResourceDoesNotExistException.MESSAGE);
     }
 
@@ -99,10 +95,9 @@ public final class GlobalErrorHandler {
      * @param e The handle exception
      * @return The error message for web user
      */
-    @ResponseBody
     @ResponseStatus(HttpStatus.ACCEPTED)
     @ExceptionHandler(ResourceNotLoadedException.class)
-    public Error handleServiceException(ResourceNotLoadedException e) {
+    public Error handle(ResourceNotLoadedException e) {
         return new Error(HttpStatus.ACCEPTED.value(), ResourceNotLoadedException.MESSAGE);
     }
 }
